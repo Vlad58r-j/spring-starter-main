969c8801c9a8126560d955301f417f990cd71ee6
Vlad Krivonos <vlad58r@gmail.com>
2026-01-28 01:18:53 +0300

    Add Liquibase and change url db

---
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -0,0 +1,15 @@
+--liquibase formated sql
+
+--changeset vladkrivonos:1
+ALTER TABLE users
+    ADD COLUMN created_at TIMESTAMP;
+
+ALTER TABLE users
+    ADD COLUMN modified_at TIMESTAMP;
+
+ALTER TABLE users
+    ADD COLUMN created_by VARCHAR(32);
+
+ALTER TABLE users
+    ADD COLUMN modified_by VARCHAR(32);
+
