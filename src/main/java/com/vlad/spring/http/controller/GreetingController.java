7e744cf923feaf043ba8245594a67a6607a2c378
Vlad Krivonos <vlad58r@gmail.com>
2026-01-30 01:42:49 +0300

    Add RequestMapping annotation

--- src/main/java/com/vlad/spring/http/controller/GreetingController.java
+++ src/main/java/com/vlad/spring/http/controller/GreetingController.java
@@ -1,17 +1,24 @@
 package com.vlad.spring.http.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/api/v1")
 public class GreetingController {
 
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET) -> менее актуально
+    @GetMapping(value = "/hello")
     public ModelAndView hello(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
     }
 
+    @GetMapping(value = "/bye")
     public ModelAndView bye() {
         var modelAndView = new ModelAndView();
         modelAndView.setViewName("greeting/bye");
