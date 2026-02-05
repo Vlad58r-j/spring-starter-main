03970a6201d5a1c446eb6bdda1614894164d5e52
Vlad Krivonos <vlad58r@gmail.com>
2026-02-05 14:47:11 +0300

    Add dynamic content for attributes

---
+++ src/main/java/com/vlad/spring/dto/UserReadDto.java
@@ -0,0 +1,10 @@
+package com.vlad.spring.dto;
+
+import lombok.Value;
+
+@Value
+public class UserReadDto {
+
+    Long id;
+    String username;
+}
--- src/main/java/com/vlad/spring/http/controller/GreetingController.java
+++ src/main/java/com/vlad/spring/http/controller/GreetingController.java
@@ -1,19 +1,37 @@
 package com.vlad.spring.http.controller;
 
+import com.vlad.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes({"user"})
 public class GreetingController {
 
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET) -> менее актуально
+    @GetMapping(value = "/hello")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+        modelAndView.setViewName("greeting/hello");
+
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/bye")
+    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
+        var modelAndView = new ModelAndView();
+        modelAndView.setViewName("greeting/bye");
+
+        return modelAndView;
+    }
+
     @GetMapping(value = "/hello/{id}")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
                               @RequestParam Integer age,
                               @RequestHeader String accept,
                               @CookieValue("JSESSIONID") String jsessionId,
@@ -22,14 +40,6 @@ public class GreetingController {
         var acceptHeader = request.getHeader("accept");
         var cookies = request.getCookies();
         modelAndView.setViewName("greeting/hello");
-
-        return modelAndView;
-    }
-
-    @GetMapping(value = "/bye")
-    public ModelAndView bye() {
-        var modelAndView = new ModelAndView();
-        modelAndView.setViewName("greeting/bye");
 
         return modelAndView;
     }
